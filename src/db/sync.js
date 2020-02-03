071b9673c2207dff915858a70fc985d68ca97220
梁书豪 <shuhao1229@163.com>
2020-02-03 16:03:11 +0800

    增加src/db src/conf src/utils文件夹 并配置sequelize实例，存储数据库的配置，并同步数据库，安装cross-env

---
+++ src/db/sync.js
@@ -0,0 +1,21 @@
+/**
+ * @description sequelize 同步数据库
+ * @author 书豪
+ */
+const seq = require('./seq');
+
+// require('./model');
+
+ // 测试连接
+seq.authenticate().then(() => {
+  console.log('auth ok')
+}).catch(() => {
+  console.log('auth error',err)
+});
+
+// 执行同步
+// force: true 表示 如果数据库有 已经存在的数据表，会删掉重新建
+seq.sync({ force: true}).then(() => {
+  console.log('sync ok')
+  process.exit();
+});
