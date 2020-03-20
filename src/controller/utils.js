8afb41c8b1563136e51adf5b1ef93fb36234268c
梁书豪 <shuhao1229@163.com>
2020-03-20 14:42:10 +0800

    feat: 上传图片 API

---
+++ src/controller/utils.js
@@ -0,0 +1,49 @@
+/**
+ * @description utils controller 
+ */
+const { ErrorModel, SuccessModel  } = require('../model/ResModel')
+const { uploadFileSizeFailInfo } = require('../model/ErrorInfo')
+const fse = require('fs-extra')
+const path = require('path')
+
+
+// 存储目录
+const DIST_FOLDER_PATH = path.join(__dirname, '..', '..', 'uploadFiles')
+// 文件最大体积 1M (1024 * kb * 比特)
+const MAX_SIZE = 1024 * 1024 * 1024
+
+// 是否需要创建目录
+fse.pathExists(DIST_FOLDER_PATH).then(exist => {
+  if (!exist) {
+    fse.ensureDir(DIST_FOLDER_PATH)
+  }
+})
+
+/**
+ * 
+ * @param {string} name 文件名
+ * @param {string} type 文件类型
+ * @param {number} size 文件体积大小
+ * @param {string} filePath 文件路径
+ */
+async function saveFile({ name, type, size, filePath }) {
+  if (size > MAX_SIZE) {
+    await fse.remove(filePath)
+    return new ErrorModel(uploadFileSizeFailInfo)
+  }
+
+  // 移动文件
+  const fileName = Date.now() + '.' + name // 防止重名
+  const distFilePath = path.join(DIST_FOLDER_PATH, fileName)
+  await fse.move(filePath, distFilePath)
+
+  // 返回信息  `/2.png`
+  return new SuccessModel({
+    url:'/' + fileName
+  })
+    
+}
+
+module.exports = {
+  saveFile
+}
---
+++ src/routes/api/utils.js
@@ -0,0 +1,26 @@
+/**
+ * @description utils api 路由
+ */
+
+const router = require('koa-router')()
+const { loginCheck } = require('../../middlewares/loginChecks')
+const koaForm = require('formidable-upload-koa')
+const { saveFile } = require('../../controller/utils')
+
+router.prefix('/api/utils')
+
+// 上传图片
+const koaFrom = require('formidable-upload-koa')
+router.post('/upload', loginCheck, koaForm(), async (ctx, next) => {
+  const file = ctx.req.files['file']
+  const { sieze, path, name, type } = file
+  // controller
+  ctx.body = await saveFile({
+    name,
+    type,
+    sieze,
+    filePath: path
+  })
+})
+
+module.exports = router
