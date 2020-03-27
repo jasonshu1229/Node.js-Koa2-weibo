2ef51c7232e53ef962b76b25fa5ce833799cabc0
梁书豪 <shuhao1229@163.com>
2020-03-27 15:38:40 +0800

    feat: 实现 at 功能

--- src/conf/constant.js
+++ src/conf/constant.js
@@ -4,5 +4,9 @@
 
 module.exports = {
   DEFAULT_PICTURE:'https://raw.githubusercontent.com/jasonshu1229/PicGo-Images/master/img/20200325183745.png',
-  PAGE_SIZE: 5
+  PAGE_SIZE: 5,
+
+  // 正则表达式，匹配 '@昵称 - userName
+  // . 匹配任意字符  +： 匹配多个  ？：贪婪匹配。匹配完立马结束  \s 遇到空格 立马结束
+  REG_FOR_AT_WHO: /@(.+?)\s-\s(\w+?)\b/g
 } 
--- src/services/_format.js
+++ src/services/_format.js
@@ -2,7 +2,7 @@
  * @description 数据格式化
  */
 
-const { DEFAULT_PICTURE } = require('../conf/constant')
+const { DEFAULT_PICTURE, REG_FOR_AT_WHO } = require('../conf/constant')
 const { timeFormat } = require('../utils//dt')
 
  /**
@@ -47,6 +47,26 @@ function _formatDBTime(obj) {
 }
 
 /**
+ * 格式化微博内容
+ * @param {Object} obj 微博数据对象
+ */
+function _formatContent(obj) {
+  obj.contentFormat = obj.content
+
+  // 格式化 @
+  // from '哈喽 @张三 - zhangsan 你好'
+  // to '哈喽 <a href="/profile/zhangsan">张三</a>你好'
+  obj.contentFormat = obj.contentFormat.replace(
+    REG_FOR_AT_WHO,
+    (matchStr, nickName, userName) => {
+      return `<a href="/profile/${userName}">@${nickName}</a>`
+    }
+  )
+
+  return obj
+}
+
+/**
  * 格式化微博信息
  * @param {Array|Object} list  微博列表或者单个微博信息
  */
@@ -57,10 +77,13 @@ function formatBlog(list) {
 
   if (list instanceof Array) {
     // 数组 
-    return list.map(_formatDBTime)
+    return list.map(_formatDBTime).map(_formatContent)
   }
   // 对象
-  return _formatDBTime(list)
+  let result = list
+  result = _formatDBTime(result)
+  result = _formatContent(result) // @格式转换链接
+  return result
 }
 
 module.exports = {
